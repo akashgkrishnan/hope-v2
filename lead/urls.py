60e8605bddb2ea0b176dba7123f2c02db8ce450c
Akash G Krishnan <krishnanag1996@gmail.com>
2020-04-21 00:42:23 +0530

    management and user forms

--- lead/urls.py
+++ lead/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('lead-home',views.lead_index, name='lead-home')
+    path('lead-home',views.lead_index, name='lead-home'),
+    path('add-management', views.create_management, name='add-management'),
+    path('add-leaduser', views.create_leaduser, name='add-leaduser')
 
 ]
--- lead/views.py
+++ lead/views.py
@@ -1,5 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+# models
+from django.contrib.auth.models import User
+from appuser.forms import UserRegisterForm
+from appuser.models import user_role_map, role_details
+# forms
+from mgmt.forms import managementForm
+from lead.forms import leadForm
+
 
 # Create your views here.
 def lead_index(request):
-    return render(request,'lead/lead-index.html')
+    return render(request, 'lead/lead-index.html')
+
+
+def create_management(request):
+    if request.method == 'POST':
+        form = managementForm(request.POST)
+        form2 = UserRegisterForm(request.POST)
+        if form.is_valid() and form2.is_valid():
+            u = form2.cleaned_data.get('username')
+            new_management = form.save(commit=False)
+            form2.save()
+            first_name = form.cleaned_data.get('first_name')
+            messages.success(request, f' {first_name} saved in system!')
+            user = User.objects.filter(username__exact=u)[0]
+            new_management.user_name = user
+            new_management.save()
+            user_role = role_details.objects.filter(role_name__exact='MANAGEMENT')[0]
+            new_user = user_role_map(stamp_user=user, role=user_role)
+            new_user.save()
+            return redirect('add-management')
+
+    else:
+        form = managementForm()
+        form2 = UserRegisterForm()
+    return render(request, 'lead/save-user.html', {'form': form, 'form2': form2, 'title': 'Add management user'})
+
+
+
+def create_leaduser(request):
+    if request.method == 'POST':
+        form = leadForm(request.POST)
+        form2 = UserRegisterForm(request.POST)
+        if form.is_valid() and form2.is_valid():
+            u = form2.cleaned_data.get('username')
+            new_lead = form.save(commit = False)
+            form2.save()
+            first_name = form.cleaned_data.get('first_name')
+            messages.success(request, f' {first_name} saved in system!')
+            user = User.objects.filter(username__exact=u)[0]
+            new_lead.user_name = user
+            new_lead.save()
+            user_role = role_details.objects.filter(role_name__exact='LEAD')[0]
+            new_user = user_role_map(stamp_user=user, role=user_role)
+            new_user.save()
+            return redirect('add-leaduser')
+
+    else:
+        form = leadForm()
+        form2 = UserRegisterForm()
+    return render(request, 'lead/save-user.html', {'form': form, 'form2': form2, 'title': 'lead user'})

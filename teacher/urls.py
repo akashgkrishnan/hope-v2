96476e1adafbafb69e91095402f63b0670f926b6
akashgkrishnan <krishnanag1996@gmail.com>
2020-05-24 22:27:56 +0530

    planners create and details and teacher home

--- teacher/urls.py
+++ teacher/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('teacher-home', views.teacher_home, name='teacher-home'),
-    path('planners', views.PlannersCreateView.as_view(), name='planners')
+    path('planners', views.PlannersCreateView.as_view(), name='planners'),
+    path('my-planners', views.my_planners,name='my-planners' ),
+    path('my-planner-details/<str:pk>', views.planners_Detail, name='my-planner-details')
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, ListView
-from .models import planners, teacher_details
-
+from .models import planners, teacher_details, teacher_subject_grade_section
 # Create your views here.
 
 
 def teacher_home(request):
-    context = {}
+    teacher = teacher_details.objects.filter(user_name=request.user)[0]
+    teacher_subjects = teacher_subject_grade_section.objects.filter(teacher=teacher)
+    context = {"my_subjects": teacher_subjects}
     return render(request, 'teacher/teacher-index.html', context)
 
 
@@ -21,3 +22,19 @@ class PlannersCreateView(LoginRequiredMixin, CreateView):
 
         form.instance.author = teacher
         return super().form_valid(form)
+
+
+def my_planners(request):
+    teacher = teacher_details.objects.filter(user_name=request.user)[0]
+    my_planners = planners.objects.filter(author=teacher)
+    context = {
+        'my_planners': my_planners
+    }
+    return render(request, 'teacher/my-planners.html', context)
+
+def planners_Detail(request, pk):
+    my_planners = planners.objects.get(id=pk)
+    context = {
+        'planner': my_planners
+    }
+    return render(request, 'teacher/my_planner_Detail.html', context)

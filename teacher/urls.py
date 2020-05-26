c6b1c0fe4be867bcd89a0f9569047fe9c30f2efb
akashgkrishnan <krishnanag1996@gmail.com>
2020-05-27 00:54:01 +0530

    slight changes to teacher UI

--- teacher/urls.py
+++ teacher/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('teacher-home', views.teacher_home, name='teacher-home'),
     path('planners', views.PlannersCreateView.as_view(), name='planners'),
     path('my-planners', views.my_planners,name='my-planners' ),
-    path('my-planner-details/<str:pk>', views.planners_Detail, name='my-planner-details')
+    path('my-planner-details/<str:pk>', views.planners_Detail, name='my-planner-details'),
+    path('grade-students/<str:pk>', views.student_gradelist, name='grade-students')
 ]

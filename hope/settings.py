6c646fa15d23e696ab1c871f9a01080983f7e146
Akash <krishnanag1996@gmail.com>
2020-06-06 16:03:09 +0000

    changes to db

--- hope/settings.py
+++ hope/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qw+gjw=wn52q&8awd%4089h5$pe2ozt1l64r^8bs7086ef(_)x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
